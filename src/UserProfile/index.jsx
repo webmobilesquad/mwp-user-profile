dec53789feda8ee1be3a112faeafc068908d243b
Kevin CELADOR <pro.kevin.celador@gmail.com>
2018-07-03 17:14:38 +0200

    The user profile component now takes a JWT in its props in order to parse user info

--- src/UserProfile/index.jsx
+++ src/UserProfile/index.jsx
@@ -1,28 +1,47 @@
 import { Component } from 'preact';
 import cx from 'classnames';
+import jwtDecode from 'jwt-decode';
 import style from './style.module.scss';
 
 export class UserProfile extends Component {
-  state = {
-    links: [
-      { name: 'Profile', url: 'https://wms-dev.michelin.net/msn/user-profile' },
-      { name: 'Logout', url: 'https://wms-dev.michelin.net/msn/logout' },
-    ],
+  constructor(props) {
+    super();
+    const { idToken } = props;
+    const { backendUrl } = props;
+    const jwtDecoded = jwtDecode(idToken);
+
+    this.setState({
+      profileUrl: { name: 'Profile', url: `https://${backendUrl}/msn/user-profile` },
+      logoutUrl: { name: 'Logout', url: `https://${backendUrl}/msn/logout` },
+      userInfo: {
+        givenName: jwtDecoded.given_name,
+        familyName: jwtDecoded.family_name,
+        email: jwtDecoded.email,
+        picture: jwtDecoded.picture,
+      },
+    });
   }
 
-  render({ className, ...props }, { links }) {
-    const { UserGivenName } = props;
-    const { UserFamilyName } = props;
-    const { UserEmail } = props;
-    const { UserPicture } = props;
+  state = {
+    profileUrl: undefined,
+    logoutUrl: undefined,
+    userInfo: {
+      picture: undefined,
+      givenName: undefined,
+      familyName: undefined,
+      email: undefined,
+    },
+  }
 
+  render({ className, ...props }, { userInfo, profileUrl, logoutUrl }) {
     return (
       <div className={cx(style.UserProfile, className)} {...props}>
-        <img src={UserPicture} alt="User pic" />
-        <h2> { UserGivenName } { UserFamilyName } </h2>
-        <h3> { UserEmail } </h3>
+        <img src={userInfo.picture} alt="User pic" />
+        <h2> {userInfo.givenName} {userInfo.familyName} </h2>
+        <h3> {userInfo.email} </h3>
         <ul>
-          {links && links.map(link => <li><a href={link.url}>{link.name}</a></li>)}
+          <li><a href={profileUrl.url}>{profileUrl.name}</a></li>
+          <li><a href={logoutUrl.url}>{logoutUrl.name}</a></li>
         </ul>
       </div>
     );
