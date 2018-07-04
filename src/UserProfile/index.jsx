f9a067f79e20642e0e25cf09962a02d0ff1b12ef
Kevin CELADOR <pro.kevin.celador@gmail.com>
2018-07-04 14:40:46 +0200

    :sparkles: UI translation The "profile" and "logout" links are now translated in the user's language - based on the "locale" JWT claim.

--- src/UserProfile/index.jsx
+++ src/UserProfile/index.jsx
@@ -1,20 +1,31 @@
 import cx from 'classnames';
 import jwtDecode from 'jwt-decode';
+import Polyglot from 'node-polyglot';
 import style from './style.module.scss';
+import messages from './messages';
+
+const initPolyglotTranslator = (locale) => {
+  let polyglotLocale = locale || 'en';
+  if (polyglotLocale.includes('-')) {
+    [polyglotLocale] = polyglotLocale.split('-');
+  }
+  const phrases = messages[polyglotLocale] || messages.en;
+  return new Polyglot({ polyglotLocale, phrases });
+};
 
 export const UserProfile = ({
   className, idToken, mwpDomain, onLogout, ...props
 }) => {
   const jwtDecoded = jwtDecode(idToken);
-  // TODO: translate a-links with polyglotJS
+  const translator = initPolyglotTranslator(jwtDecoded.locale);
   return (
     <div className={cx(style.UserProfile, className)} {...props}>
-      <img src={jwtDecoded.picture} alt="User pic" />
+      <img src={jwtDecoded.picture} alt="user avatar" />
       <h2> {jwtDecoded.given_name} {jwtDecoded.family_name} </h2>
       <h3> {jwtDecoded.email} </h3>
       <ul>
-        <li><a href={`https://${mwpDomain}/msn/user-profile`}>Profile</a></li>
-        <li><a onClick={onLogout} href={`https://${mwpDomain}/msn/logout`}>Logout</a></li>
+        <li><a href={`https://${mwpDomain}/msn/user-profile`}>{translator.t('profile')}</a></li>
+        <li><a onClick={onLogout} href={`https://${mwpDomain}/msn/logout`}>{translator.t('logout')}</a></li>
       </ul>
     </div>
   );
---
+++ src/UserProfile/messages.js
@@ -0,0 +1,30 @@
+export default {
+  en: {
+    profile: 'Profile',
+    logout: 'Logout',
+  },
+  fr: {
+    profile: 'Profil',
+    logout: 'Déconnexion',
+  },
+  de: {
+    profile: 'Benutzerprofil',
+    logout: 'Abschaltung',
+  },
+  es: {
+    profile: 'Perfil',
+    logout: 'Desconexión',
+  },
+  pt: {
+    profile: 'Perfil',
+    logout: 'Desconexão',
+  },
+  it: {
+    profile: 'Profilo',
+    logout: 'Esci',
+  },
+  ru: {
+    profile: 'Profil\'',
+    logout: 'Vykhod iz sistemy',
+  },
+};
