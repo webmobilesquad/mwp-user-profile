e6b2f177e1925b3f9b36f6c73d396275ea262738
Kevin CELADOR <pro.kevin.celador@gmail.com>
2018-07-05 18:22:09 +0200

    :lipstick:  UserProfile component design (button and menu)

--- src/UserProfile/index.jsx
+++ src/UserProfile/index.jsx
@@ -1,3 +1,4 @@
+import { Component } from 'preact';
 import cx from 'classnames';
 import jwtDecode from 'jwt-decode';
 import Polyglot from 'node-polyglot';
@@ -13,20 +14,66 @@ const initPolyglotTranslator = (locale) => {
   return new Polyglot({ polyglotLocale, phrases });
 };
 
-export const UserProfile = ({
-  className, idToken, mwpDomain, onLogout, ...props
-}) => {
-  const jwtDecoded = jwtDecode(idToken);
-  const translator = initPolyglotTranslator(jwtDecoded.locale);
+function UserProfileMenu({
+  jwt, mwpDomain, onLogout, visible, ...props
+}) {
+  const translator = initPolyglotTranslator(jwt.locale);
   return (
-    <div className={cx(style.UserProfile, className)} {...props}>
-      <img src={jwtDecoded.picture} alt="user avatar" />
-      <h2> {jwtDecoded.given_name} {jwtDecoded.family_name} </h2>
-      <h3> {jwtDecoded.email} </h3>
+    <div
+      className={cx(style.UserProfile_Menu, { [style.UserProfile_Menu__visible]: visible })}
+      {...props}
+    >
+      <header>
+        <img src={jwt.picture} alt="user avatar" />
+        <div>
+          <h2>
+            {jwt.given_name} {jwt.family_name}
+          </h2>
+          <h3>{jwt.email}</h3>
+        </div>
+      </header>
       <ul>
-        <li><a href={`https://${mwpDomain}/msn/user-profile`}>{translator.t('profile')}</a></li>
-        <li><a onClick={onLogout} href={`https://${mwpDomain}/msn/logout`}>{translator.t('logout')}</a></li>
+        <li>
+          <a href={`https://${mwpDomain}/msn/user-profile`}>{translator.t('profile')}</a>
+        </li>
+        <li>
+          <a onClick={onLogout} href={`https://${mwpDomain}/msn/logout`}>
+            {translator.t('logout')}
+          </a>
+        </li>
       </ul>
     </div>
   );
-};
+}
+
+export class UserProfile extends Component {
+  state = {
+    menuVisible: false,
+  };
+
+  handleClick = () => {
+    const { menuVisible } = this.state;
+    this.setState({ menuVisible: !menuVisible });
+  };
+
+  render({
+    className, idToken, mwpDomain, onLogout, ...props
+  }, { menuVisible }) {
+    const jwt = jwtDecode(idToken);
+
+    return (
+      <div className={cx(style.UserProfile, className)} {...props}>
+        <button type="button" onClick={this.handleClick} className={cx(style.UserProfile_Button)}>
+          <img src={jwt.picture} alt="user avatar" />
+        </button>
+        {/* TODO: hide UserProfileMenu when clicking elswhere */}
+        <UserProfileMenu
+          jwt={jwt}
+          mwpDomain={mwpDomain}
+          onLogout={onLogout}
+          visible={menuVisible}
+        />
+      </div>
+    );
+  }
+}
